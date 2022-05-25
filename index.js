6b6748ce5244ab370a8343d0a94da73e2d0fe7c7
lamartm <spider_mo@hotmail.nl>
2022-05-25 14:24:44 +0200

    test

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static("public"));
-
+console.log("hey")
 app.get("/", async (req, res) => {
   const document = await client.getAllByType("teacher-card");
   res.render("page", { document, currentDate });
