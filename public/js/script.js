378ee09229480e1db2c2440d325dba0c8cd6a750
Mohamed <spider_mo@hotmail.nl>
2022-06-06 21:41:29 +0200

    added keyboard and changed styling to fit better with style

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,23 @@
+const Keyboard = window.SimpleKeyboard.default;
+
 const teachers = document.getElementsByClassName("teacher");
+const keyboardSimple = document.getElementById("keyboard");
+const keyboardBackground = document.getElementById("keyboard-bg");
+
+const keyboard = new Keyboard({
+  onChange: (input) => onChange(input),
+  onKeyPress: (button) => onKeyPress(button),
+});
+
+document.getElementById("search-input").addEventListener("click", (d) => {
+  keyboardBackground.style.display = "initial";
+  keyboardSimple.style.display = "initial";
+});
+
+keyboardBackground.addEventListener("click", (d) => {
+  keyboardBackground.style.display = "none";
+  keyboardSimple.style.display = "none";
+});
 
 document.getElementById("search-teacher").addEventListener("submit", (d) => {
   d.preventDefault();
@@ -6,7 +25,7 @@ document.getElementById("search-teacher").addEventListener("submit", (d) => {
     element.classList.remove("found");
     if (
       element.textContent.trim().toLowerCase() ==
-      d.target[0].value.toLowerCase()
+      d.target[1].value.toLowerCase()
     ) {
       element.className += " found";
       element.scrollIntoView({ behavior: "smooth" });
@@ -15,17 +34,32 @@ document.getElementById("search-teacher").addEventListener("submit", (d) => {
   });
 });
 
+document.querySelector(".input").addEventListener("input", (event) => {
+  keyboard.setInput(event.target.value);
+});
+
+console.log(keyboard);
+
+function onChange(input) {
+  document.querySelector(".input").value = input;
+  console.log("Input changed", input);
+}
+
+function onKeyPress(button) {
+  console.log("Button pressed", button);
+}
+
 // Scrollen
 function goPrecious() {
-  document.querySelector('html').scrollBy({ 
+  document.querySelector("html").scrollBy({
     top: -320,
-    behavior: 'smooth' 
+    behavior: "smooth",
   });
 }
 
 function goNext() {
-  document.querySelector('html').scrollBy({ 
+  document.querySelector("html").scrollBy({
     top: 320,
-    behavior: 'smooth' 
+    behavior: "smooth",
   });
 }
